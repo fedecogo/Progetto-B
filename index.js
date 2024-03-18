6c6f9789851c0ba75ec67150aef77b280121d14d
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-03-18 10:43:31 +0100

    Player foto, logica per i movimenti e switch immagine, correzzione dell indentazione

--- index.js
+++ index.js
@@ -6,38 +6,84 @@ canvas.height = 576
 
 const gravity = 1
 
+const PlayerRunRight = new Image()
+PlayerRunRight.src = './img/PlayerRunRight.png'
+
+const PlayerRunLeft = new Image()
+PlayerRunLeft.src = './img/PlayerRunLeft.png'
+
+const PlayerStandRight = new Image()
+PlayerStandRight.src = './img/PlayerStandRight.png'
+
+const PlayerStandLeft = new Image()
+PlayerStandLeft.src = './img/PlayerStandLeft.png'
 
 class Player {
     constructor() {
-        this.speed =15
+        this.speed = 15
         this.position = {
-            x:100,
-            y:100
+            x: 100,
+            y: 100
         }
         this.velocity = {
-            x:0,
-            y:1
+            x: 0,
+            y: 1
         }
-        this.width = 50
-        this.height= 50
+        this.width = 66
+        this.height = 150
+        this.image = PlayerStandRight
+        this.frame = 0
+        this.players = {
+            stand: {
+                right: PlayerStandRight,
+                left: PlayerStandLeft,
+                cropWidth: 177,
+                width: 66
+            },
+            run: {
+                right: PlayerRunRight,
+                left: PlayerRunLeft,
+                cropWidth: 341,
+                width: 127.875
+            }
+        }
+        this.currentPlayer = this.players.stand.right
+        this.currentCropWidth = 177
     }
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y,
-            this.width,this.height)
-    }   
-    update(){
-        this.draw() 
+        c.drawImage(
+            this.currentPlayer,
+            this.currentCropWidth * this.frame,
+            0,
+            this.currentCropWidth,
+            400,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height)
+    }
+    update() {
+        this.frame++
+
+        if (this.frame > 59 &&
+            (this.currentPlayer === this.players.stand.right ||
+                this.currentPlayer === this.players.stand.left))
+            this.frame = 0
+        else if (this.frame > 29 &&
+            (this.currentPlayer === this.players.run.right ||
+                this.currentPlayer === this.players.run.left))
+            this.frame = 0
+
+        this.draw()
         this.position.y += this.velocity.y
         this.position.x += this.velocity.x
-        if(this.position.y + this.height+ this.velocity.y <= canvas.height)
-        this.velocity.y += gravity
-        
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += gravity
     }
 }
 
 class Platform {
-     constructor({x,y,image}) {
+    constructor({ x, y, image }) {
         this.position = {
             x,
             y
@@ -46,24 +92,24 @@ class Platform {
         this.width = image.width
         this.height = image.height
     }
-    draw(){
-        c.drawImage(this.image , this.position.x , this.position.y)
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
 class scenarioObject {
-    constructor({x,y,image}) {
-       this.position = {
-           x,
-           y
-       }
-       this.image = image
-       this.width = image.width
-       this.height = image.height
-   }
-   draw(){
-       c.drawImage(this.image , this.position.x , this.position.y)
-   }
+    constructor({ x, y, image }) {
+        this.position = {
+            x,
+            y
+        }
+        this.image = image
+        this.width = image.width
+        this.height = image.height
+    }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
 }
 
 
@@ -72,13 +118,17 @@ let platforms = []
 
 let image = new Image()
 image.onload = () => {
-    platforms.push(new Platform({ x:  -20, y: 520,image}))
-    platforms.push(new Platform({ x: image.width - 20, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *2 -20, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *4 -40, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *5 -40, y: 520,image}))
-    // platforms.push(new Platform({ x: 200, y: 100, image }))
-    // platforms.push(new Platform({ x: 400, y: 200, image }))
+    platforms.push(new Platform({ x: -2, y: 520, image }))
+    platforms.push(new Platform({ x: image.width - 20, y: 520, image }))
+    platforms.push(new Platform({ x: image.width * 2 - 20, y: 520, image }))
+    platforms.push(new Platform({ x: image.width * 4 - 40, y: 520, image }))
+    platforms.push(new Platform({ x: image.width * 5 - 40, y: 520, image }))
+    platforms.push(new Platform({ x: image.width * 7 - 20, y: 520, image }))
+    platforms.push(new Platform({ x: image.width * 8 - 40, y: 520, image }))
+    platforms.push(new Platform({ x: image.width * 11 - 40, y: 520, image }))
+    platforms.push(new Platform({ x: image.width * 12 - 20, y: 520, image }))
+    platforms.push(new Platform({ x: image.width * 14 - 40, y: 520, image }))
+    platforms.push(new Platform({ x: image.width * 15 - 80, y: 520, image }))
 }
 image.src = './img/platform.png'
 
@@ -90,159 +140,184 @@ imageSO.src = './img/backGround.jpg'
 let imageTR = new Image()
 imageTR.src = './img/hills.png'
 imageSO.onload = () => {
-    // scenarioObjects.push(new scenarioObject({x:0, y:0,image:imageSO}))
-    scenarioObjects.push(new scenarioObject({x:0, y:250,image:imageTR}))
-    scenarioObjects.push(new scenarioObject({x:250, y:300,image:imageTR}))
+    scenarioObjects.push(new scenarioObject({ x: 0, y: 250, image: imageTR }))
+    scenarioObjects.push(new scenarioObject({ x: 250, y: 300, image: imageTR }))
 }
 
+function init() {
+    player = new Player()
+    platforms = []
 
-function init (){
- player = new Player()
- platforms = []
+    image = new Image()
+    image.onload = () => {
+        platforms.push(new Platform({ x: -2, y: 520, image }))
+        platforms.push(new Platform({ x: image.width - 20, y: 520, image }))
+        platforms.push(new Platform({ x: image.width * 2 - 20, y: 520, image }))
+        platforms.push(new Platform({ x: image.width * 4 - 40, y: 520, image }))
+        platforms.push(new Platform({ x: image.width * 5 - 40, y: 520, image }))
+        platforms.push(new Platform({ x: image.width * 7 - 20, y: 520, image }))
+        platforms.push(new Platform({ x: image.width * 8 - 40, y: 520, image }))
+        platforms.push(new Platform({ x: image.width * 11 - 40, y: 520, image }))
+        platforms.push(new Platform({ x: image.width * 12 - 20, y: 520, image }))
+        platforms.push(new Platform({ x: image.width * 14 - 40, y: 520, image }))
+        platforms.push(new Platform({ x: image.width * 15 - 80, y: 520, image }))
+    }
+    image.src = './img/platform.png'
 
- image = new Image()
-image.onload = () => {
-    platforms.push(new Platform({ x:  -20, y: 520,image}))
-    platforms.push(new Platform({ x: image.width - 20, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *2 -20, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *4 -40, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *5 -40, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *7 -20, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *8 -40, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *11 -40, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *12 -20, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *14 -40, y: 520,image}))
-    platforms.push(new Platform({ x: image.width *15 -80, y: 520,image}))
-    
-    // platforms.push(new Platform({ x: 200, y: 100, image }))
-    // platforms.push(new Platform({ x: 400, y: 200, image }))
-}
-image.src = './img/platform.png'
+    scenarioObjects = []
 
-
- scenarioObjects = []
-
- imageSO = new Image()
-imageSO.src = './img/backGround.jpg'
- imageTR = new Image()
-imageTR.src = './img/hills.png'
-imageSO.onload = () => {
-    // scenarioObjects.push(new scenarioObject({x:0, y:0,image:imageSO}))
-    scenarioObjects.push(new scenarioObject({x:0, y:250,image:imageTR}))
-    scenarioObjects.push(new scenarioObject({x:250, y:300,image:imageTR}))
+    imageSO = new Image()
+    imageSO.src = './img/backGround.jpg'
+    imageTR = new Image()
+    imageTR.src = './img/hills.png'
+    imageSO.onload = () => {
+        scenarioObjects.push(new scenarioObject({ x: 0, y: 250, image: imageTR }))
+        scenarioObjects.push(new scenarioObject({ x: 250, y: 300, image: imageTR }))
+    }
 }
 
-}
-
-
+let lastKey
 const keys = {
-    right:{
-        pressed: false 
+    right: {
+        pressed: false
     },
-    left:{
-        pressed: false 
+    left: {
+        pressed: false
     }
 }
 
 let scrollOffSet = 0
 
-function aniamte(){
-    requestAnimationFrame(aniamte)
-    c.clearRect(0,0, canvas.width, canvas.height)
-    scenarioObjects.forEach(scenarioObject =>{
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    scenarioObjects.forEach(scenarioObject => {
         scenarioObject.draw()
     })
     platforms.forEach((platform) => {
-         platform.draw()  
+        platform.draw()
     })
-  
-     player.update()
-    if(keys.right.pressed && player.position.x <500){
+    player.update()
+
+    if (keys.right.pressed && player.position.x < 500) {
         player.velocity.x = player.speed
     } else if (
-        (keys.left.pressed && player.position.x > 100 )    || 
+        (keys.left.pressed && player.position.x > 100) ||
         (keys.left.pressed && scrollOffSet === 0 && player.position.x > 0)
-     ) { player.velocity.x = -player.speed}
-    else{
-       player.velocity.x = 0 
-       if (keys.right.pressed){
-        scrollOffSet += player.speed
-        platforms.forEach((platfrom) => {
-            platfrom.position.x -= player.speed
-       })
-       scenarioObjects.forEach(scenarioObject =>{
-        scenarioObject.position.x -= player.speed *.66
-       })
-      
-       } else if (keys.left.pressed && scrollOffSet > 0){
-        scrollOffSet -= player.speed
-        platforms.forEach((platfrom) => {
-            platfrom.position.x += player.speed
-        })
-        scenarioObjects.forEach(scenarioObject =>{
-            scenarioObject.position.x += player.speed *.66
-           })
-
-       }
-    } 
+    ) {
+        player.velocity.x = -player.speed
+    } else {
+        player.velocity.x = 0
+        if (keys.right.pressed) {
+            scrollOffSet += player.speed
+            platforms.forEach((platfrom) => {
+                platfrom.position.x -= player.speed
+            })
+            scenarioObjects.forEach(scenarioObject => {
+                scenarioObject.position.x -= player.speed * .66
+            })
+        } else if (keys.left.pressed && scrollOffSet > 0) {
+            scrollOffSet -= player.speed
+            platforms.forEach((platfrom) => {
+                platfrom.position.x += player.speed
+            })
+            scenarioObjects.forEach(scenarioObject => {
+                scenarioObject.position.x += player.speed * .66
+            })
+        }
+    }
 
     platforms.forEach((platform) => {
-       //platform collitions 
-       if(player.position.y + player.height <= platform.position.y 
-        && player.position.y + player.height + player.velocity.y >= platform.position.y 
-        && player.position.x + player.width >= platform.position.x 
-        && player.position.x <= platform.position.x + platform.width){
-        player.velocity.y = 0 
-    }
-   })
+        if (player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+    })
 
-   // win condition
-   if(scrollOffSet > 2000){
-    console.log("you win")
-   }
-   // lose condition
-   if (player.position.y >= canvas.height){
-    init()
-   }
+    if (
+        keys.right.pressed &&
+        lastKey === 'right' &&
+        player.currentPlayer !== player.players.run.right
+    ) {
+        player.frame = 1
+        player.currentPlayer = player.players.run.right
+        player.currentCropWidth = player.players.run.cropWidth
+        player.width = player.players.run.width
+    } else if (
+        keys.left.pressed &&
+        lastKey === 'left' &&
+        player.currentPlayer !== player.players.run.left
+    ) {
+        player.currentPlayer = player.players.run.left
+        player.currentCropWidth = player.players.run.cropWidth
+        player.width = player.players.run.width
+    } else if (
+        !keys.left.pressed &&
+        lastKey === 'left' &&
+        player.currentPlayer !== player.players.stand.left
+    ) {
+        player.currentPlayer = player.players.stand.left
+        player.currentCropWidth = player.players.stand.cropWidth
+        player.width = player.players.stand.width
+    }
+    // hai vinto
+    if (scrollOffSet > 2000) {
+        console.log("you win")
+    }
+    //hai perso 
+    if (player.position.y >= canvas.height) {
+        init()
+        scrollOffSet = 0;
+    }
 }
 
-aniamte()
+animate()
 
-window.addEventListener('keydown',({keyCode})=>{
-    switch(keyCode){
-        case 37 :
+window.addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+        case 37:
             console.log('left')
             keys.left.pressed = true
+            lastKey = 'left'
             break
-        case 38 :
+        case 38:
             console.log('up')
             player.velocity.y -= 20
             break
-        case 40 :
+        case 40:
             console.log('down')
             break
-        case 39 :
+        case 39:
             console.log('right')
             keys.right.pressed = true
-            break    
+            lastKey = 'right'
+            break
     }
 })
 
-window.addEventListener('keyup',({keyCode})=>{
-    switch(keyCode){
-        case 37 :
+window.addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+        case 37:
             console.log('left')
             keys.left.pressed = false
+            player.currentPlayer = player.players.stand.left
+            player.currentCropWidth = player.players.stand.cropWidth
+            player.width = player.players.stand.width
             break
-        case 38 :
+        case 38:
             console.log('up')
             break
-        case 40 :
+        case 40:
             console.log('down')
             break
-        case 39 :
+        case 39:
             console.log('right')
             keys.right.pressed = false
-            break    
+            player.currentPlayer = player.players.stand.right
+            player.currentCropWidth = player.players.stand.cropWidth
+            player.width = player.players.stand.width
+            break
     }
 })
